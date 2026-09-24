4593589756478b89875fd45e21d3e86129b010fc
YuribSantos008 <benicioyuri0@gmail.com>
2026-09-24 21:16:56 +0000

    cria estrutura inicial do projeto

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,24 @@ app.use(cors());
 app.use(express.json());
 
 const produtos = [
-        {
-            nome: "Teclado Mecânico",
-            preco: 249.90,
-            categoria: "Periféricos"
-        },
-        {
-            nome: "Mouse Gamer",
-            preco: 149.90,
-            categoria: "Periféricos"
-        },
-        {
-            nome: "Monitor Full HD",
-            preco: 899.90,
-            categoria: "Monitores"
-        }
+    {
+        nome: "Teclado Mecânico",
+        preco: 249.90,
+        categoria: "Periféricos",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx3EdAAzuFDxrj2sEpTDIPNyCAt73gxwFL8dqL1SSTWw&s=10"
+    },
+    {
+        nome: "Mouse Gamer",
+        preco: 149.90,
+        categoria: "Periféricos",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqdzqlu6HjM-U0gJy0dyq0U9hpGssZzNchaB_Tr85T5A&s=10"
+    },
+    {
+        nome: "Monitor Full HD",
+        preco: 899.90,
+        categoria: "Monitores",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWe_LmCIRlP3fXaw26kYsLH5jHaHuf9Sj30Lz2cv9cZA&s=10"
+    }
 ];
  app.get("/", (req, res) => {
     res.json(produtos);
